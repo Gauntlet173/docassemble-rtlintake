d08e2d0d972a8665a8c043310776a88a55de7291
Jason Morris <jason@roundtablelaw.ca>
2020-04-01 18:37:35 -0400

    changing behaviour of closing screen for lawyer

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.rtlintake',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.rtlintake\n\nA docassemble extension.\n\n## Author\n\nJason Morris, jason@roundtablelaw.ca\n\n',
+      long_description='# docassemble.rtlintake\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nJason Morris, jason@roundtablelaw.ca\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Jason Morris',
       author_email='jason@roundtablelaw.ca',
